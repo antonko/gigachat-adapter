b0745d2ddb34aa48b43f4725cb1d704b5dc6fac3
Anton Kovalev <antonko@gmail.com>
2024-12-29 22:48:49 +0300

    tests: исправлена ошибка передачи env-значения при запуске тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,11 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 
-from src.main import get_application
-
 TEST_BEARER_TOKEN = "test_token"
 os.environ["BEARER_TOKEN"] = TEST_BEARER_TOKEN
+
+
+from src.main import get_application  # noqa: E402
 
 
 @pytest.fixture(scope="function")
