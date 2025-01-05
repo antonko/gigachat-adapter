9112b59e3dff6a2d5e470d2f6fb4c2d68220d9fe
Anton Kovalev <antonko@gmail.com>
2025-01-06 02:32:43 +0300

    fix: пояснение за monkey patch

--- src/gigachat_monkey_patch.py
+++ src/gigachat_monkey_patch.py
@@ -1,3 +1,10 @@
+"""
+Этот "хак" (монки-патчинг) необходим для корректной работы потокового получения данных из GigaChat.
+Проблема заключается в том, что библиотека httpx по умолчанию не использует HTTP/2,
+что приводит к тому, что GigaChat не отправляет данные потоками, а возвращает их сразу целиком.
+Данный код исправляет это поведение, включая поддержку HTTP/2 при подключении к API GigaChat.
+"""
+
 from typing import Any, Dict
 
 import gigachat.client
@@ -11,7 +18,7 @@ def _get_kwargs(settings: Settings) -> Dict[str, Any]:
         "base_url": settings.base_url,
         "verify": settings.verify_ssl_certs,
         "timeout": httpx.Timeout(settings.timeout),
-        "http2": True,
+        "http2": True,  # Включаем поддержку HTTP/2
     }
     if settings.ca_bundle_file:
         kwargs["verify"] = settings.ca_bundle_file
